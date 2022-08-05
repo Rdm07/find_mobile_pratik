e37e999dcad91e0b7cc54476935d929da5a0cfa6
Rohan Dharmadhikari <drohan.1994@gmail.com>
2022-08-05 12:09:33 +0530

    runs in linux

--- run_training.bat
+++
@@ -1,3 +0,0 @@
-@REM Batch file to run training
-
-python train_find_mobile_Resnet18_pre_trained.py --data "D:\VSCode_Workspace\find_mobile_pratik\data"
---
+++ run_training.sh
@@ -0,0 +1,3 @@
+DATA_FOLDER=/mnt/vol_d/VSCode_Workspace/find_mobile_pratik/data
+
+python train_find_mobile_Resnet18_pre_trained.py --data ${DATA_FOLDER}
