2379e0fbc1710a1dc6ff668feb989be4dfad32b3
Rohan Dharmadhikari <drohan.1994@gmail.com>
2022-08-22 13:45:00 +0530

    changed file name

--- run_training.sh
+++ run_training.sh
@@ -1,3 +1,3 @@
 DATA_FOLDER=/mnt/vol_d/VSCode_Workspace/find_mobile_pratik/data
 
-python train_find_mobile_Resnet18_pre_trained.py --data ${DATA_FOLDER}
+python train_model.py --data ${DATA_FOLDER}
